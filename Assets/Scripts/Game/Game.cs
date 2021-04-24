7473d165c376445553fa0bb332988cbbad08dc6e
mavlac <martin.x.vlach@gmail.com>
2021-04-24 16:13:24 +0200

    Slowly building the scene

---
+++ Assets/Scripts/Game/Game.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Game : MonoBehaviour
+{
+	public const float GridSize = 0.16f;
+}
---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour, InputControls.IGameplayActions
+{
+	public void OnMovement(InputAction.CallbackContext context)
+	{
+		Vector2 controlVector;
+		controlVector = context.ReadValue<Vector2>();
+	}
+}
